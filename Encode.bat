21a8ffb7cd64b4154ee83f200bed63007e610647
sano <sano@netartz.com>
2016-09-13 20:11:58 +0900

    順序の見直し、avs2wav排除、パラメータ指定で実写とアニメを分離

--- Encode.bat
+++ Encode.bat
@@ -1,43 +1,52 @@
 @echo off
 
-rem -----------------------------------------------------------------------
-rem 初期設定
-rem -----------------------------------------------------------------------
+REM -----------------------------------------------------------------------
+REM x264パラメータ (クォリティ = アニメ19～20, 実写21～23 程度)
+REM -----------------------------------------------------------------------
+REM set quality=20
+set quality=23
+REM -----------------------------------------------------------------------
+REM x264パラメータ (アスペクト比 = 1:1 or リサイズ済み, 16:9, 4:3)
+REM -----------------------------------------------------------------------
+set aspect=1:1
+REM set aspect=32:27
+REM set aspect=8:9
+REM -----------------------------------------------------------------------
+REM x264パラメータ (ソースタイプ = 実写, アニメ)
+REM -----------------------------------------------------------------------
+set source_type=--psy-rd 0.6:0 --trellis 1
+REM set source_type=--psy-rd 0.2:0 --trellis 2
+
+REM -----------------------------------------------------------------------
+set x264=--crf %quality% --sar %aspect% --level 4.1 --qpmin 10 --qcomp 0.8 --scenecut 50 --min-keyint 1 --direct auto --weightp 1 --bframes 4 --b-adapt 2 --b-pyramid normal --ref 4 --rc-lookahead 50 --qpstep 4 --aq-mode 2 --aq-strength 0.80 --me umh --subme 9 %source_type% --no-fast-pskip --no-dct-decimate
+
+REM -----------------------------------------------------------------------
+REM プログラムフォルダと出力先フォルダ
+REM -----------------------------------------------------------------------
 set program_dir=C:\DTV\Encode\
 set output_dir=F:\
 
-rem -----------------------------------------------------------------------
-rem プログラムファイル名
-rem -----------------------------------------------------------------------
-set x264_path="%program_dir%x264_64.exe"
+REM -----------------------------------------------------------------------
+REM プログラムファイル名
+REM -----------------------------------------------------------------------
+set x264_path="%program_dir%x264_x64.exe"
 set wavi_path="%program_dir%wavi_x64.exe"
 set qaac_path="%program_dir%qaac64.exe"
 set muxer_path="%program_dir%muxer.exe"
 set remuxer_path="%program_dir%remuxer.exe"
 
-rem -----------------------------------------------------------------------
-rem x264パラメータ
-rem -----------------------------------------------------------------------
-REM set quality=20
-set quality=23
-set aspect=1:1
-REM set aspect=32:27
-REM set aspect=8:9
-rem -----------------------------------------------------------------------
-set x264=--crf %quality% --sar %aspect% --level 4.1 --bframes 4 --b-adapt 2 --b-pyramid normal --ref 4 --rc-lookahead 50 --qpstep 4 --aq-mode 2 --aq-strength 0.80 --me umh --subme 9 --psy-rd 0.6:0 --trellis 1 --no-fast-pskip --no-dct-decimate --threads 0 --thread-input
-
-rem -----------------------------------------------------------------------
-rem ループ処理の開始
-rem -----------------------------------------------------------------------
+REM -----------------------------------------------------------------------
+REM ループ処理の開始
+REM -----------------------------------------------------------------------
 :loop
 if "%~1" == "" goto end
 
 set input_avs="%~1"
 set filename=%~n1
 
-rem -----------------------------------------------------------------------
-rem 出力ファイル名
-rem -----------------------------------------------------------------------
+REM -----------------------------------------------------------------------
+REM 出力ファイル名
+REM -----------------------------------------------------------------------
 set output_enc="%output_dir%%filename%.enc.mp4"
 set output_wav="%output_dir%%filename%.wav"
 set output_aac="%output_dir%%filename%.aac"
@@ -84,10 +93,10 @@ echo.
 echo ======================================================================
 echo 一時ファイル削除
 echo ======================================================================
-REM del /f /q %output_mp4%
-REM del /f /q %output_wav%
-REM del /f /q %output_aac%
-REM del /f /q %output_m4a%
+del /f /q %output_mp4%
+del /f /q %output_wav%
+del /f /q %output_aac%
+del /f /q %output_m4a%
 if not exist %output_mp4% echo %output_mp4%
 if not exist %output_wav% echo %output_wav%
 if not exist %output_aac% echo %output_aac%
