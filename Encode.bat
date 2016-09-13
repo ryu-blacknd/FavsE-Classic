e75ccdcf84fac8d3b03d8d0ceb63e8711c7a6ce8
sano <sano@netartz.com>
2016-09-13 16:39:26 +0900

    Avisynth+ 64bit用に一応落ち着いた

--- Encode.bat
+++ Encode.bat
@@ -3,20 +3,17 @@
 rem -----------------------------------------------------------------------
 rem 初期設定
 rem -----------------------------------------------------------------------
-set program_path=C:\DTV\Encode\
-set temp_path=F:\
-set comp_path=F:\
+set program_dir=C:\DTV\Encode\
+set output_dir=F:\
 
 rem -----------------------------------------------------------------------
-rem プログラム・ファイルのパス
+rem プログラムファイル名
 rem -----------------------------------------------------------------------
-set avs4x26x_path="%program_path%avs4x26x.exe"
-set x264_path="%program_path%x264_64.exe"
-REM set wavi_path="%program_path%wavi.exe"
-set avs2wav_path="%program_path%avs2wav32.exe"
-set qaac_path="%program_path%qaac.exe"
-set muxer_path="%program_path%muxer.exe"
-set remuxer_path="%program_path%remuxer.exe"
+set x264_path="%program_dir%x264_64.exe"
+set wavi_path="%program_dir%wavi_x64.exe"
+set qaac_path="%program_dir%qaac64.exe"
+set muxer_path="%program_dir%muxer.exe"
+set remuxer_path="%program_dir%remuxer.exe"
 
 rem -----------------------------------------------------------------------
 rem x264パラメータ
@@ -27,7 +24,7 @@ set aspect=1:1
 REM set aspect=32:27
 REM set aspect=8:9
 rem -----------------------------------------------------------------------
-set x264=--crf %quality% --sar %aspect% --level 4.1 --qpmin 0 --qpmax 69 --qpstep 4 --qcomp 0.75 --aq-mode 1 --aq-strength 1 --psy-rd 1:0 --bframes 3 --ref 3 --b-adapt 2 --b-pyramid "normal" --weightb --mixed-refs --direct "auto" --me "umh" --subme 9 --merange 16 --trellis 2 --8x8dct --partitions "p8x8,b8x8,i8x8,i4x4" --keyint 240 --min-keyint 24 --scenecut 60 --weightp 1 --threads 0 --thread-input
+set x264=--crf %quality% --sar %aspect% --level 4.1 --bframes 4 --b-adapt 2 --b-pyramid normal --ref 4 --rc-lookahead 50 --qpstep 4 --aq-mode 2 --aq-strength 0.80 --me umh --subme 9 --psy-rd 0.6:0 --trellis 1 --no-fast-pskip --no-dct-decimate --threads 0 --thread-input
 
 rem -----------------------------------------------------------------------
 rem ループ処理の開始
@@ -36,16 +33,16 @@ rem -----------------------------------------------------------------------
 if "%~1" == "" goto end
 
 set input_avs="%~1"
-set name=%~n1
+set filename=%~n1
 
 rem -----------------------------------------------------------------------
-rem 出力・入力パス
+rem 出力ファイル名
 rem -----------------------------------------------------------------------
-set output_mp4="%temp_path%%name% [enc].mp4"
-set output_wav="%temp_path%%name%.wav"
-set output_aac="%temp_path%%name%.aac"
-set output_m4a="%temp_path%%name%.m4a"
-set complete_mp4="%comp_path%%name%.mp4"
+set output_enc="%output_dir%%filename%.enc.mp4"
+set output_wav="%output_dir%%filename%.wav"
+set output_aac="%output_dir%%filename%.aac"
+set output_m4a="%output_dir%%filename%.m4a"
+set output_mp4="%output_dir%%filename%.mp4"
 
 echo ======================================================================
 echo 処理開始
@@ -54,27 +51,22 @@ echo %input_avs%
 echo.
 
 echo ======================================================================
-echo x264でエンコード
+echo x264で映像エンコード
 echo ======================================================================
-%avs4x26x_path% -L %x264_path% %x264% -o %output_mp4% %input_avs%
+%x264_path% %x264% -o %output_enc% %input_avs%
+REM %
 echo.
 
-REM echo ======================================================================
-REM echo waviで音声出力(wav)
-REM echo ======================================================================
-REM %wavi_path% %input_avs% %output_wav%
-REM echo.
-
 echo ======================================================================
-echo avs2wavで音声出力(wav)
+echo waviで音声出力(wav)
 echo ======================================================================
-%avs2wav_path% %input_avs% %output_wav%
+%wavi_path% %input_avs% %output_wav%
 echo.
 
 echo ======================================================================
 echo qaacで音声エンコード(aac)
 echo ======================================================================
-%qaac_path% -q 2 --abr 128 --ignorelength %output_wav% -o %output_aac%
+%qaac_path% -q 2 --abr 192 --ignorelength %output_wav% -o %output_aac%
 echo.
 
 echo ======================================================================
@@ -86,16 +78,16 @@ echo.
 echo ======================================================================
 echo L-SMASHで映像・音声remux
 echo ======================================================================
-%remuxer_path% -i %output_mp4% -i %output_m4a% -o %complete_mp4%
+%remuxer_path% -i %output_enc% -i %output_m4a% -o %output_mp4%
 echo.
 
 echo ======================================================================
 echo 一時ファイル削除
 echo ======================================================================
-del /f /q %output_mp4%
-del /f /q %output_wav%
-del /f /q %output_aac%
-del /f /q %output_m4a%
+REM del /f /q %output_mp4%
+REM del /f /q %output_wav%
+REM del /f /q %output_aac%
+REM del /f /q %output_m4a%
 if not exist %output_mp4% echo %output_mp4%
 if not exist %output_wav% echo %output_wav%
 if not exist %output_aac% echo %output_aac%
--- MakeAVS.bat
+++ MakeAVS.bat
@@ -1,6 +1,10 @@
+REM 2016.09.12 AviSynth+ 64bit版用に書き直し
+
 @echo off
 
-set logos_path=C:\DTV\Encode\logos\
+set files_dir=C:\DTV\Encode\
+set logos_dir=C:\DTV\Encode\logos\
+set plugins_dir=C:\Program Files (x86)\AviSynth+\plugins64+\
 
 :loop
 if "%~1" == "" goto end
@@ -11,13 +15,15 @@ echo %avs_file%
 echo avsource = %1>> %avs_file%
 echo.>> %avs_file%
 
+echo SetMemoryMax(2048)>> %avs_file%
+echo Import("%files_dir%MT.avsi")>> %avs_file%
+echo.>> %avs_file%
+
 echo ### LSMASHSourceでの読み込み ###>> %avs_file%
 echo #LWLibavVideoSource(avsource, dr=true, repeat=true, dominance=1, fpsnum=30000, fpsden=1001)>> %avs_file%
 echo LWLibavVideoSource(avsource, dr=true, repeat=true, dominance=1).AssumeFPS(30000, 1001)>> %avs_file%
 echo AudioDub(last, LWLibavAudioSource(avsource, av_sync=true, layout="stereo"))>> %avs_file%
-echo.>> %avs_file%
-
-echo SetMTMode(2, 0)>> %avs_file%
+echo if (height() == 1088) { Crop(0, 0, 0, -8) }>> %avs_file%
 echo.>> %avs_file%
 
 echo ### DirectShowSourceでの読み込み ###>> %avs_file%
@@ -28,18 +34,15 @@ echo ### AviSourceでの読み込み ###>> %avs_file%
 echo #AviSource(avsource)>> %avs_file%
 echo.>> %avs_file%
 
-echo ### LWLibavVideoSourceでdr=trueを指定した場合の高さ調整 ###>> %avs_file%
-echo (height() == 1088) ? Crop(0, 0, 0, -8) : SetMTMode(2)>> %avs_file%
-echo.>> %avs_file%
-
-echo ### 字幕処理(idx+sub) ###>> %avs_file%
+echo ### 字幕処理(idx+sub形式の字幕ファイルを指定) ###>> %avs_file%
 echo #VobSub("")>> %avs_file%
 echo.>> %avs_file%
 
 echo ### カット編集 ###>> %avs_file%
+echo #Trim(0, 0)>> %avs_file%
 echo.>> %avs_file%
 
-echo ### 放送局ロゴファイル指定（ロゴ除去を行う場合のみ） ###>> %avs_file%
+echo ### 放送局ロゴファイル指定(ロゴ除去を行う場合のみコメントアウト) ###>> %avs_file%
 echo logoname = "">> %avs_file%
 echo #logoname = "NHK BS1.lgd">> %avs_file%
 echo #logoname = "NHK BSプレミアム.lgd">> %avs_file%
@@ -67,9 +70,9 @@ echo #logoname = "ＣＢＣテレビ.lgd">> %avs_file%
 echo #logoname = "メ～テレ.lgd">> %avs_file%
 echo.>> %avs_file%
 
-echo ### ロゴ除去（logonameが指定されている場合のみ） ###>> %avs_file%
-echo logofile = (logoname != "") ? "C:\DTV\Encode\logos\" + logoname : "">> %avs_file%
-echo (logofile != "") ? EraseLOGO(logofile=logofile, pos_x=0, pos_y=0, depth=128, yc_y=0, yc_u=0, yc_v=0, start=0, fadein=0, fadeout=0, end=-1, interlaced=true) : SetMTMode(2)>> %avs_file%
+echo ### ロゴ除去(logonameが指定されている場合のみ実行される) ###>> %avs_file%
+echo logofile = (logoname != "") ? "%logos_dir%" + logoname : "">> %avs_file%
+echo if (logofile != "") { EraseLOGO(logofile=logofile, pos_x=0, pos_y=0, depth=128, yc_y=0, yc_u=0, yc_v=0, start=0, fadein=0, fadeout=0, end=-1, interlaced=true) }>> %avs_file%
 echo.>> %avs_file%
 
 echo ### クロップ ###>> %avs_file%
@@ -80,12 +83,11 @@ echo ### 塗りつぶし(ロゴファイルが無い場合) ###>> %avs_file%
 echo #Letterbox(116, 0)>> %avs_file%
 echo.>> %avs_file%
 
-echo #AssumeTFF()>> %avs_file%
+echo AssumeTFF()>> %avs_file%
 echo.>> %avs_file%
 
-echo ### 逆テレシネ + インターレース解除(後者は軽い) ###>> %avs_file%
+echo ### 逆テレシネ + インターレース解除 ###>> %avs_file%
 echo TIVTC24P2()>> %avs_file%
-echo #TDeint(mode=0, order=1, type=3, tryweave=true).TDecimate(mode=1, hybrid=0)>> %avs_file%
 echo.>> %avs_file%
 
 echo ### 逆テレシネのみ ###>> %avs_file%
@@ -103,18 +105,17 @@ echo #Spline36Resize(854, 480)>> %avs_file%
 echo #Spline36Resize(640, 480)>> %avs_file%
 echo.>> %avs_file%
 
-echo ### アップコンバート(SD->HD拡大) ###>> %avs_file%
+echo ### アップコンバート(SD->HD拡大時、別に上記リサイズでも良い) ###>> %avs_file%
 echo #nnedi3_rpow2(rfactor=2, nsize=4, nns=0, qual=1, cshift="spline36resize", fwidth=1280, fheight=720, ep0=4)>> %avs_file%
 echo.>> %avs_file%
 
-echo ### ノイズ除去 ###>> %avs_file%
+echo ### ノイズ除去(後者は軽く実写向け) ###>> %avs_file%
 echo #FFT3DFilter(sigma=1.5, beta=1, plane=4, bw=32, bh=32, ow=16, oh=16, bt=3, sharpen=0, interlaced=false, wintype=0)>> %avs_file%
 echo #hqdn3d(2)>> %avs_file%
 echo.>> %avs_file%
 
 echo ### シャープ化 ###>> %avs_file%
-echo #Import("C:\Program Files (x86)\AviSynth\plugins\LSFmod.v1.9.avsi");>> %avs_file%
-echo #SetMTMode(6)>> %avs_file%
+echo #Import("%plugins_dir%LSFmod.v1.9.avsi")>> %avs_file%
 echo #LSFmod(defaults="slow", strength=40, dest_x=1280, dest_y=720)>> %avs_file%
 echo.>> %avs_file%
 
